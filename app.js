b2fad4fea45ac9c024328ea8c8c60a0f5b006362
Gohil-Parikshitsinh <parikshitsinhgohil03@gmail.com>
2025-02-02 14:23:20 +0530

    DELETE Route

--- app.js
+++ app.js
@@ -70,6 +70,14 @@ app.put("/listings/:id", async (req,res)=>{
   res.redirect(`/listings/${id}`);
 })
 
+//Delete Route
+app.delete("/listings/:id", async (req,res)=>{
+  let {id} = req.params;
+  const deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings")
+  
+})
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
