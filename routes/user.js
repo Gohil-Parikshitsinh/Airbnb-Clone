b5a9fc01bb575f9a87c14ad54e2ffaf5f078e2df
Gohil-Parikshitsinh <parikshitsinhgohil03@gmail.com>
2025-04-20 19:03:23 +0530

    post-login-page

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
@@ -37,6 +38,7 @@ router.get("/login", (req, res) => {
 
 router.post(
   "/login",
+  saveRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
@@ -44,7 +46,8 @@ router.post(
   async (req, res) => {
     req.flash("success",`Welcome ${req.body.username}`)
     // req.flash("success","Welcome to wanderlust")
-    res.redirect("/listings");
+    let redirectUrl = res.locals.redirectUrl || "/listings"
+    res.redirect(redirectUrl);
   }
 );
 
